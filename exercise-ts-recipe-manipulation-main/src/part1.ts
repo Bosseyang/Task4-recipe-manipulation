08334051deaffe1a99d1410830c5d70cb88f1709
Bosseyang <bosse_yang@hotmail.com>
2025-08-13 16:45:08 +0200

    Update part1.ts
    Done down to question 6.

--- exercise-ts-recipe-manipulation-main/src/part1.ts
+++ exercise-ts-recipe-manipulation-main/src/part1.ts
@@ -5,12 +5,14 @@ export function part1() {
     console.log(`1. Answer: ${recipeName.innerHTML}`);
   }
   // 1. Answer: Frozen Cheescake
+
   // ------------------------------------------------------------------------------------
   //2. What HTML tag is used to display the Recipe name?
   if (recipeName) {
     console.log(`2. Answer: ${recipeName.tagName}`);
   }
   // 2. Answer: H1
+
   // ------------------------------------------------------------------------------------
   // 3. What is the font size of the paragraph tag with the class "description".
   const descriptionEl =
@@ -20,31 +22,44 @@ export function part1() {
     console.log(`3. Answer: ${fontSize}`);
   }
   // 3. Answer: 16px
+
   // ------------------------------------------------------------------------------------
   // 4. What is the value of the alt atrribute on the image?
   const imgEl = document.querySelector<HTMLImageElement>(
     'img[src="/src/assets/frozen-cheesecake-slice.jpg"]'
   );
   if (imgEl) {
-    console.log(`3. Answer: ${imgEl.alt}`);
+    console.log(`4. Answer: ${imgEl.alt}`);
   }
   // Answer: Slice of Frozen Cheescake
+
   // ------------------------------------------------------------------------------------
   // 5. What is the dimensions and the url of the image? Create an object that looks like this, and log it to the console:
   if (imgEl) {
     const imgInfo = {
       url: imgEl.src,
-      height: imgEl.naturalHeight,
-      width: imgEl.naturalWidth,
+      height: imgEl.height,
+      width: imgEl.width,
     };
-    console.log(`3. Answer: ${imgInfo}`);
+    console.log(
+      `5. Answer: url:${imgInfo.url} height: ${imgEl.height}, width: ${imgEl.width}`
+    );
   }
+  // Answer: url:http://localhost:5173/src/assets/frozen-cheesecake-slice.jpg height: 608, width: 908
 
   // ------------------------------------------------------------------------------------
   // 6. How many ingredients has the paste?
+  const ingredientsEl = document.querySelectorAll<HTMLUListElement>(
+    ".ingredients-list-paste li"
+  );
+  if (ingredientsEl) {
+    console.log(`6. Answer: ${ingredientsEl.length}`);
+  }
+  // Answer: 5
 
   // ------------------------------------------------------------------------------------
   // 7. Which is the forth element in the list containing the ingredients for the paste?
+  
 
   // ------------------------------------------------------------------------------------
   // 8. Create an an array of objects from the instructions. Each element in the array should be an object that looks like this:
