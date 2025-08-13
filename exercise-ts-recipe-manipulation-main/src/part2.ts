ea5edd12ce5f6e250ec40fbbb4f437198261dc95
Bosseyang <bosse_yang@hotmail.com>
2025-08-13 17:43:20 +0200

    Update part2.ts
    Task 5 done, timer icon fixed.

--- exercise-ts-recipe-manipulation-main/src/part2.ts
+++ exercise-ts-recipe-manipulation-main/src/part2.ts
@@ -24,6 +24,12 @@ export function part2() {
   }
   // ------------------------------------------------------------------------------------
   // 5. The clock icon beneath the recipe name has disappeared and been replaced by a text instead. This can be fixed by adding a class to that element.
+  const timerIconEl = document.querySelector<HTMLSpanElement>(
+    ".time-container span"
+  );
+  if (timerIconEl) {
+    timerIconEl.classList.add("material-symbols-outlined");
+  }
   // ------------------------------------------------------------------------------------
   // 6. The estimated time of the recipe is also incorrect. Change it to the correct time estimation.
   // ------------------------------------------------------------------------------------
